ef7c2f17b950fa1514ba15c9d763ff436da41f49
Adair Cruz Guerrero <77763586+adaircg1719@users.noreply.github.com>
2021-06-15 13:26:14 -0500

    Update activacion.js

--- js/activacion.js
+++ js/activacion.js
@@ -74,47 +74,14 @@ async function verifica() {
 */
 
 async function activarCliente() {
-    const ClienteToken= {
-      digito1: document.getElementById("Code1").value,
-      digito2: document.getElementById("Code2").value,
-      digito3: document.getElementById("Code3").value,
-      digito4: document.getElementById("Code4").value,
-      digito5: document.getElementById("Code5").value,
-      digito6: document.getElementById("Code6").value,
+    const digito1= document.getElementById("Code1").value
+    const digito2= document.getElementById("Code2").value
+    const digito3= document.getElementById("Code3").value
+    const digito4= document.getElementById("Code4").value
+    const digito5= document.getElementById("Code5").value
+    const digito6= document.getElementById("Code6").value
 
+    const ClienteToken= {
       token:digito1+digito2+digito3+digito4+digito5+digito6
     }
-    
-    console.log(ClienteToken);
-    await axios 
-      .post("https://back-activacion.herokuapp.com/api/activacion/token", ClienteToken) //url de servicio post y la const del html 
-    
-      .then(res => {  // Revisa la respuesta del POST
-
-        if (res.request.status == 200) {
-        var retorno = res.data;   // el resultado del query llega a res.data
-        console.log (retorno[0].resultado); 
-      }
-      
-      if (retorno[0].resultado == 0 ) {
-        console.log ('El token no es correcto verifiquelo')
-        window.alert("El token no es correcto verifiquelo");
-        // alertify.error("El RFC no existe en la base");
-        // alertify.set('notifier','position', 'top-center');
-        // alertify.error("Ups! No tenemos ese RFC registrado");
-        alertify.dialog('Ups!').set({transition:'pulse',message: 'Ups! el token no es correcto verifiquelo'}).show();
-      } 
-      else {
-        if (retorno[0].resultado > 0 ) {
-          console.log ('Token verificado')
-      }
-    }
-      return retorno;
-    })
-
-      .catch(err => {
-        if (err.request.status == 404) {
-            window.alert("Problema con la petición");
-        }
-      })
 };
